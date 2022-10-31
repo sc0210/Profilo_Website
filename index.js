7cd84caae60c3fac153fea795a0deec0d103b9a9
SamChen <sam88210@gmail.com>
2022-10-31 15:17:27 +0800

    update core

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-/* Toggle between adding and removing the "responsive" class to when the user clicks on the icon */
-function myFunction() {
-    var x = document.getElementById("navbar");
-    if (x.className === "navbar-text") {
-        x.className += " responsive";
-        console.log(x.className)
-    } else {
-        x.className = "navbar-text";
-        console.log(x.className)
-    }
-}
+const http = require('http')
+
+const server = http.createServer(function (req, res){
+    res.writeHead(200, {'Content-Type': 'text/plain'})
+    res.end('Hello world')
+})
+
+server.listen('3000', function () {
+    console.log('server start on 3000 port')
+})
